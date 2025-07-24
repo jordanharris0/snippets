efd2c1aa82cd629c32cda2c370fc7a4e1c0ebe00
jordanharris0 <jordanharris1833@yahoo.com>
2025-07-24 15:47:39 -0500

    finished user routes and workshop

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from typing import Optional
 import json
 import os
 from cryptography.fernet import Fernet
+from cryptography.fernet import InvalidToken
 import bcrypt
 
 #dot env
@@ -26,21 +27,27 @@ class Snippet(BaseModel):
     language: str
     code: str
 
+class User(BaseModel):
+    email: str
+    password: str
+
 # load data file
-def load_snippets():
+def load_data():
     if os.path.exists(DATA_FILE):
         with open(DATA_FILE, 'r', encoding="utf-8") as f:
             return json.load(f)
-    return []
+    return {"snippets": [], "users": []}
 
 # save snippets to file
-def save_snippets(snippets):
+def save_data(data):
     with open(DATA_FILE, 'w', encoding="utf-8") as f:
-        json.dump(snippets, f, indent=2)
+        json.dump(data, f, indent=2)
 
 
 # initalize snippet list
-snippets = load_snippets()
+data = load_data()
+snippets = data["snippets"]
+users = data["users"]
 
 # post route
 @app.post('/snippets')
@@ -60,7 +67,10 @@ def create_snippet(snippet: Snippet):
 
     #append data to snippets list and save to file
     snippets.append(snippet_data)
-    save_snippets(snippets)
+    save_data({
+    "snippets": snippets,
+    "users": users
+    })
     return {"message": "Snippet saved successfully", "snippet": snippet_data}
 
 # get all route
@@ -71,32 +81,83 @@ def get_all_snippets():
         s["code"] = fernet.decrypt(s["code"].encode()).decode()
     return snippets
 
-# get snippet by id
-@app.get('/snippets/{snippet_id}')
-def get_snippet(snippet_id: int):
-    for s in snippets:
-        if s["id"] == snippet_id:
-            return {
-                "id": s["id"],
-                "language": s["language"],
-                "code": fernet.decrypt(s["code"].encode()).decode()
-            }
-        
-    return {'error': 'Snippets not found'}
 
 # get snippets by language
-@app.get('/snippets')
+@app.get('/snippets/language')
 def get_snippet_lang(language: str = None):
+    result = []
+
+    if not language:
+        return []
 
     #loop though snippets
     for s in snippets:
         #if language in snippets = language lowercase
         if s["language"].lower() == language.lower():
-            #decrypt "code"
-            decrypted_code = fernet.decrypt(s["code"].encode()).decode()
+            try:
+                #decrypt "code"
+                decrypted_code = fernet.decrypt(s["code"].encode()).decode()
+            except InvalidToken:
+                decrypted_code = "<decryption failed: invalid token>"
             #return values of language
+            result.append( {
+                "id": s["id"],
+                "language": s["language"],
+                "code": decrypted_code
+            })
+
+    return result
+
+
+# get snippet by id
+@app.get('/snippets/{snippet_id}')
+def get_snippet(snippet_id: int):
+    for s in snippets:
+        if s["id"] == snippet_id:
+            try:
+                decrypted_code = fernet.decrypt(s["code"].encode()).decode()
+            except InvalidToken:
+                return {
+                    "id": s["id"],
+                    "language": s["language"],
+                    "code": "<decryption failed: invalid token>"
+                }
+                
             return {
                 "id": s["id"],
                 "language": s["language"],
                 "code": decrypted_code
             }
+        
+    return {'error': 'Snippets not found'}
+
+
+
+@app.post('/user')
+def create_user(user: User):
+    #hash the password
+    hashed_password = bcrypt.hashpw(user.password.encode(), bcrypt.gensalt())
+
+    #save user to file and append to snippets and save to seedData.json
+    user_data = {
+        "email": user.email,
+        "password": hashed_password.decode()
+    }
+    users.append(user_data)
+    save_data({
+    "snippets": snippets,
+    "users": users
+    })
+
+    return {"message": "User created successfully", "user": user_data}
+
+@app.get('/user/by-login')
+def get_user(email: str, password: str):
+
+    for user in users:
+        if user["email"] == email:
+            #check if passwords match
+            if bcrypt.checkpw(password.encode(), user["password"].encode()):
+                return {"message": "Login successful", "user": email}
+            else:
+                return {"error": "Invalid password"}
